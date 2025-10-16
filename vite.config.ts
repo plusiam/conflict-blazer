cec534ac636757d784ee749bf417d35b32ab40b6
plusiam <yeohanki@naver.com>
2025-10-16 21:44:34 +0900

    Deploy to main branch /docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/conflict-blazer/",
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "::",
     port: 8080,
